a2c61f8476d1c3ada067d776a5342cc4ecda678e
danjan09 <janzen.daniel@gmail.com>
2021-08-03 12:57:11 +0200

    Mittagspause

--- script_01.js
+++ script_01.js
@@ -2,11 +2,60 @@
 
 
 
-/*** 01. Funktionalität mit Einzelparametern */
+/** Theorie: Schleifen **/
+
+// for (let i = 1; i <= 10; i++) {
+//     output(i);
+// }
+
+// let arr = ["Ich","bin", "der", "coole", "Max", "Mütze"]
+// for (let i = 0; i < arr.length; i++) {
+//     // output(i);
+//     output(arr[i]);
+// }
+
+
+/*** 2a. Funktionalität mit Array 1 ***/
+
+// output(getSentence("Ich","bin","Max","Mütze"));
+function getSentence(arr) {
+    const gap = " ";
+    const punct = "."
+    const str = arr[0] + gap + 
+                arr[1] + gap +
+                arr[2] + gap +
+                arr[3] +
+                punct;
+    return str; 
+}
+    // --> Transformation  semantisches Problem --> numerisches Problem
+
+
+
+/** Theorie: Array **/
+
+// let arr;
+// arr = new Array();  // Konstuktor
+// arr = [];           // Literal
+// arr = [2,4,7,12];
+// arr = [true,false];
+// arr = ["Ich","bin","Max","Mütze"];
+
+
+// output(arr);
+// output(arr.length);
+// output(arr[0]);    // Index, beginnt IMMER mit 0 !!!
+// output(arr[3]);
+// output(arr[arr.length-1]);
+
+
+
+/*** 1. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 
 // Modul: Satz-String zusammensetzen
-ausgabe(getSentence("Ich","bin","Max","Mütze"));
+
+// output(getSentence("Ich","bin","Max","Mütze"));
 function getSentence(word1,word2,word3,word4) {
     const gap = " ";
     const punct = "."
@@ -20,8 +69,9 @@ function getSentence(word1,word2,word3,word4) {
 
 
 // Modul: Ausgabe in Konsole : Test
+
 // ausgabe("hi"); 
-function ausgabe(outputStr) {
+function output(outputStr) {
     console.log(outputStr);
 }
 
